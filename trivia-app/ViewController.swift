bf8bcedb42a2aedd76e0b41c3d55cf06d9920ff3
Rafael Villarreal <rvillarreal416@gmail.com>
2020-01-12 00:20:08 -0500

    [feat]: Trying to extend to a different types of buttons

--- trivia-app/ViewController.swift
+++ trivia-app/ViewController.swift
@@ -11,21 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     
     var bottomCustomButton = CustomButton()
+    var accentButton = AccentButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         setupBottomButtonConstraints()
+        setupTopButtonConstraints()
     }
     
     func setupBottomButtonConstraints() {
         view.addSubview(bottomCustomButton)
         bottomCustomButton.translatesAutoresizingMaskIntoConstraints = false
-        bottomCustomButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        bottomCustomButton.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        bottomCustomButton.heightAnchor.constraint(equalToConstant: 53).isActive = true
+        bottomCustomButton.widthAnchor.constraint(equalToConstant: 315).isActive = true
         bottomCustomButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         bottomCustomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -200).isActive = true
+    }
+    
+    func setupTopButtonConstraints() {
+        view.addSubview(accentButton)
+        accentButton.translatesAutoresizingMaskIntoConstraints = false
+        accentButton.heightAnchor.constraint(equalToConstant: 53).isActive = true
+        accentButton.widthAnchor.constraint(equalToConstant: 315).isActive = true
+        accentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        accentButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -400).isActive = true
+        
     }
 }
 
