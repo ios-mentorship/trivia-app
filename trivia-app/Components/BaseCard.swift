0d6c5c11e9f2456f5e799689d42720862518cc0b
Rafael Villarreal <rvillarreal416@gmail.com>
2020-01-14 23:57:51 -0500

    [feat]: initial file for base card component

---
+++ trivia-app/Components/BaseCard.swift
@@ -0,0 +1,21 @@
+//
+//  BaseCard.swift
+//  trivia-app
+//
+//  Created by Rafael Villarreal on 14/01/20.
+//  Copyright © 2020 Rafael Villarreal. All rights reserved.
+//
+
+import UIKit
+
+class BaseCard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
